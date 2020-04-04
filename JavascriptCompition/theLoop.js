ef1727d0ef077b16a3d8b61bfa0966d7b6ab7680
Doge <mastrdoge@gmail.com>
2020-04-04 13:04:03 +0100

    Square formations

--- JavascriptCompition/theLoop.js
+++ JavascriptCompition/theLoop.js
@@ -1,8 +1,11 @@
 let keyList = [];
 let score = 0;
 let squareList = [];
-
+let timer = 0;
+let difficulty = 1;
 let gameover = false;
+let squareGap = -60;
+let squarePos = 74.5;
 
 function setup() {
 	createCanvas(1400, 400);
@@ -31,14 +34,148 @@ function draw() {
 		document.getElementById("scoreText").innerText = "Score: " + score;
 
 		if (keyList.includes(" ")) {
-			squareList.push(new Square(-90, 185, 10))
+			squareList.push(new Square(-90, 185, 10));
 		}
+
+		timer++;
+		if (timer > 60) { //larger the number the easier the game is
+			sendSquares();
+			timer = 0;
+		}
+
+
 	} else {
 		//in gamemover menu
 		if (keyList.includes("r")) {
 
 			reset();
 		}
+	}
+}
+
+function sendSquares() {
+	let formationNo = Math.floor(Math.random() * 20);
+	switch (formationNo) {
+		case -1:
+			squareList.push(new Square(squareGap * 1, squarePos * 0, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 1, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 2, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 3, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 4, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 5, 10));
+			break;
+		case 0: //top gap leaning right going right
+			squareList.push(new Square(squareGap * 1, squarePos * 2, 10));
+			squareList.push(new Square(squareGap * 2, squarePos * 3, 10));
+			squareList.push(new Square(squareGap * 3, squarePos * 4, 10));
+			squareList.push(new Square(squareGap * 4, squarePos * 5, 10));
+			break;
+		case 1: //top gap leaning left going right
+			squareList.push(new Square(squareGap * 4, squarePos * 2, 10));
+			squareList.push(new Square(squareGap * 3, squarePos * 3, 10));
+			squareList.push(new Square(squareGap * 2, squarePos * 4, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 5, 10));
+			break;
+		case 2: //bottom gap leaning right going right
+			squareList.push(new Square(squareGap * 1, squarePos * 0, 10));
+			squareList.push(new Square(squareGap * 2, squarePos * 1, 10));
+			squareList.push(new Square(squareGap * 3, squarePos * 2, 10));
+			squareList.push(new Square(squareGap * 4, squarePos * 3, 10));
+			break;
+		case 3: //bottom gap leaning left going right
+			squareList.push(new Square(squareGap * 4, squarePos * 0, 10));
+			squareList.push(new Square(squareGap * 3, squarePos * 1, 10));
+			squareList.push(new Square(squareGap * 2, squarePos * 2, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 3, 10));
+			break;
+		case 4: //middle gap leaning left going right
+			squareList.push(new Square(squareGap * 1, squarePos * 0, 10));
+			squareList.push(new Square(squareGap * 2, squarePos * 1, 10));
+			squareList.push(new Square(squareGap * 5, squarePos * 4, 10));
+			squareList.push(new Square(squareGap * 6, squarePos * 5, 10));
+			break;
+		case 5: //middle gap leaning right going right
+			squareList.push(new Square(squareGap * 6, squarePos * 0, 10));
+			squareList.push(new Square(squareGap * 5, squarePos * 1, 10));
+			squareList.push(new Square(squareGap * 2, squarePos * 4, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 5, 10));
+			break;
+		case 6: //big gap going right
+			squareList.push(new Square(squareGap * 1, squarePos * 0, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 5, 10));
+			break;
+		case 7: //medium gap going right
+			squareList.push(new Square(squareGap * 1, squarePos * 1, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 4, 10));
+			break;
+		case 8: //small gap going right
+			squareList.push(new Square(squareGap * 1, squarePos * 2, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 3, 10));
+			break;
+		case 9: //classic going right
+			squareList.push(new Square(squareGap * 1, squarePos * 2, 10));
+			squareList.push(new Square(squareGap * 1, squarePos * 3, 10));
+			squareList.push(new Square(squareGap * 1.5, squarePos * 1, 10));
+			squareList.push(new Square(squareGap * 1.5, squarePos * 4, 10));
+			squareList.push(new Square(squareGap * 2, squarePos * 2, 10));
+			squareList.push(new Square(squareGap * 2, squarePos * 3, 10));
+			break;
+		case 10: //top gap leaning left going left
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 2, -10));
+			squareList.push(new Square(squareGap * -2 + window.width, squarePos * 3, -10));
+			squareList.push(new Square(squareGap * -3 + window.width, squarePos * 4, -10));
+			squareList.push(new Square(squareGap * -4 + window.width, squarePos * 5, -10));
+			break;
+		case 11: //top gap leaning right going left
+			squareList.push(new Square(squareGap * -4 + window.width, squarePos * 2, -10));
+			squareList.push(new Square(squareGap * -3 + window.width, squarePos * 3, -10));
+			squareList.push(new Square(squareGap * -2 + window.width, squarePos * 4, -10));
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 5, -10));
+			break;
+		case 12: //bottom gap leaning left going left
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 0, -10));
+			squareList.push(new Square(squareGap * -2 + window.width, squarePos * 1, -10));
+			squareList.push(new Square(squareGap * -3 + window.width, squarePos * 2, -10));
+			squareList.push(new Square(squareGap * -4 + window.width, squarePos * 3, -10));
+			break;
+		case 13: //bottom gap leaning right going left
+			squareList.push(new Square(squareGap * -4 + window.width, squarePos * 0, -10));
+			squareList.push(new Square(squareGap * -3 + window.width, squarePos * 1, -10));
+			squareList.push(new Square(squareGap * -2 + window.width, squarePos * 2, -10));
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 3, -10));
+			break;
+		case 14: //middle gap leaning right going left
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 0, -10));
+			squareList.push(new Square(squareGap * -2 + window.width, squarePos * 1, -10));
+			squareList.push(new Square(squareGap * -5 + window.width, squarePos * 4, -10));
+			squareList.push(new Square(squareGap * -6 + window.width, squarePos * 5, -10));
+			break;
+		case 15: //middle gap leaning left going left
+			squareList.push(new Square(squareGap * -6 + window.width, squarePos * 0, -10));
+			squareList.push(new Square(squareGap * -5 + window.width, squarePos * 1, -10));
+			squareList.push(new Square(squareGap * -2 + window.width, squarePos * 4, -10));
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 5, -10));
+			break;
+		case 16: //big gap going left
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 0, -10));
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 5, -10));
+			break;
+		case 17: //medium gap going left
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 1, -10));
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 4, -10));
+			break;
+		case 18: //small gap going left
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 2, -10));
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 3, -10));
+			break;
+		case 19: //classic going left
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 2, -10));
+			squareList.push(new Square(squareGap * -1 + window.width, squarePos * 3, -10));
+			squareList.push(new Square(squareGap * -1.5 + window.width, squarePos * 1, -10));
+			squareList.push(new Square(squareGap * -1.5 + window.width, squarePos * 4, -10));
+			squareList.push(new Square(squareGap * -2 + window.width, squarePos * 2, -10));
+			squareList.push(new Square(squareGap * -2 + window.width, squarePos * 3, -10));
+			break;
 	}
 }
 
